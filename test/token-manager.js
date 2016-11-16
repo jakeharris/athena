6d0ccaceb9def0a532e1d9041f7ab48f3010a789
Jake Harris <milieu@javakat.io>
2016-11-15 22:06:33 -0600

    ECMA6ify the TokenManager tests

--- test/token-manager.js
+++ test/token-manager.js
@@ -10,16 +10,16 @@ describe('TokenManager', function () {
   context('constructor', function () {
     it('prepares to read a file in the parent directory named "tokens" if not supplied with parameters', function () {
       assert.doesNotThrow(function () {
-        var tm = new TokenManager()
+        let tm = new TokenManager()
       })
-      var tm = new TokenManager()
+      let tm = new TokenManager()
       assert.equal(tm.filePath, '../tokens')
     })
     it('prepares to read a file with the given path if supplied with a filePath', function () {
       assert.doesNotThrow(function () {
-        var tm = new TokenManager('example-tokens/empty-tokens')
+        let tm = new TokenManager('example-tokens/empty-tokens')
       })
-      var tm = new TokenManager('example-tokens/empty-tokens')
+      let tm = new TokenManager('example-tokens/empty-tokens')
       assert.equal(tm.filePath, 'example-tokens/empty-tokens')
     })
   })
@@ -31,41 +31,18 @@ describe('TokenManager', function () {
     it('throws an error if any of the name-token pairs are invalid') 
     it('exposes an associative array of API tokens if successful, where each key is the name of the service', function () {
       assert.doesNotThrow(function () {
-        var tm = new TokenManager().parseTokens()
+        let tm = new TokenManager().parseTokens()
       })
-      var tm = new TokenManager().onTokensLoaded(function () {
+      let tm = new TokenManager().parseTokens().then(function () {
         assert.notEqual(tm.tokens['discord'], undefined)
-      }).parseTokens()
+      })
       
       assert.doesNotThrow(function () {
-        var tm = new TokenManager().parseTokens()
+        let tm = new TokenManager().parseTokens()
       })
-      var tm = new TokenManager().onTokensLoaded(function () {
+      tm = new TokenManager().parseTokens().then(function () {
         assert.equal(tm.tokens['discord'], 'loremIpsum19391jfjjAf8') 
-      }).parseTokens()
-    })
-  })
-  context('onTokensLoaded()', function () {
-    var tm
-    beforeEach(function () {
-      tm = new TokenManager()
-    })
-    it('throws an error if it receives no parameters', function () {
-      assert.throws(function () {
-        tm.onTokensLoaded()
       })
-    })
-    it('throws an error if it does not receive a callback function', function () {
-      assert.throws(function () {
-        tm.onTokensLoaded(4)
-      })
-    })
-    it('registers the given callback function so that it will be called when the tokens are loaded', function () {
-      var f = function () { return 4 }
-      assert.doesNotThrow(function () {
-        tm.onTokensLoaded(f)
-      })
-      assert.equal(tm.tokensLoadedCallback(), 4)
     })
   })
 })
