8741b7b78a28bdd99a9488db37af0cba294a0b17
Jake Harris <milieu@javakat.io>
2017-01-08 16:06:06 -0600

    Enable responses from Athena!

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const Discord = require('discord.js'),
-    TokenManager = require('./src/token-manager')
+const Discord      = require('discord.js'),
+      TokenManager = require('./src/token-manager'),
+      Request      = require('request')
 
 let athena = new Discord.Client(),
     tm = new TokenManager('tokens')
 
 athena.on('ready', () => {
     console.log('Recalling Overwatch members...')
+}).on('message', (message) => {
+    if(message.content.indexOf('/ow') === 0) {
+        let tags = message.content.split(' ').slice(1)
+        message.channel.sendMessage('Your id is: ' + message.channel.id)
+        Request.post({
+            url: 'http://localhost:3000/post',
+            json: true,
+            body: { tags: tags, id: parseInt(message.channel.id) }
+        }, (error, response, body) => {
+            if(error) message.channel.sendMessage(error)
+            else {
+                message.channel.sendMessage(body)
+                    .catch(console.log);
+            }
+        })
+    }
 })
 
 tm.parseTokens().then(() => {
